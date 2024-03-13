2dc48bbdb4469480c23369886c422fa94bfb818f
Rapcha01 <jackyrapcha1301@gmail.com>
2024-03-13 09:48:36 +0300

    updated and created a new search component

---
+++ src/components/search-autocomplete api/index.jsx
@@ -0,0 +1,61 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function SearchAutocomplete() {
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+  const [searchParam, setSearchParam] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  function handleChange(event) {
+    const query = event.target.value.toLowerCase();
+    setSearchParam(query);
+    if (query.length > 1) {
+      const filteredData =
+        users && users.length
+          ? users.filter((item) => item.lowerCase().indexof(query) > -1)
+          : [];
+      setFilteredUsers(filteredData);
+      setShowDropdown(true);
+    } else {
+      setShowDropdown(false);
+    }
+  }
+
+  async function fetchListOfUsers() {
+    try {
+      setLoading(true);
+      const response = await fetch("https://dummyjson.com/users");
+      const data = await response.json();
+
+      if (data && data.users && data.users.length) {
+        setUsers(data.users.map((userItem) => userItem.firstName));
+        setLoading(false);
+        setError(null);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+      setError(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchListOfUsers();
+  }, []);
+
+  console.log(users, filteredUsers);
+
+  return (
+    <div className="search-autocomplete">
+      <input
+        value={searchParam}
+        name="search-users"
+       placeholder=" Search users here..."
+        onchange={handleChange}
+      />
+    </div>
+  );
+}
